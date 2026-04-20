a5c5f52bbdcd69612f5c6bfc617dbe482e7fb2bf
Josh14 <joshua.huizer@gmail.com>
2026-04-20 18:05:34 +0200

    Update MiniInstaller.sh
    Updated wget commands to use quiet mode for cleaner output.

--- MiniInstaller.sh
+++ MiniInstaller.sh
@@ -13,17 +13,17 @@ And if you encounter any bugs, please contact me by commenting on the GitHub Rep
 EOF
 read -p "Do you want to install MiniEditor? (Y/N)" INPUT
 if [[ "${INPUT,,}" == "y" ]]; then
-wget -O /usr/local/bin/MiniEditor https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniEditor
+wget -q -O /usr/local/bin/MiniEditor https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniEditor
 chmod 555 /usr/local/bin/MiniEditor
 fi
 read -p "Do you want to install MiniMath? (Y/N)" INPUT
 if [[ "${INPUT,,}" == "y" ]]; then
-wget -O /usr/local/bin/MiniMath https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniMath
+wget -q -O /usr/local/bin/MiniMath https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniMath
 chmod 555 /usr/local/bin/MiniMath
 fi
 read -p "Do you want to install MiniPass? (Y/N)" INPUT
 if [[ "${INPUT,,}" == "y" ]]; then
-wget -O /usr/local/bin/MiniPass https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniPass
+wget -q -O /usr/local/bin/MiniPass https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniPass
 chmod 555 /usr/local/bin/MiniPass
 fi
 echo "Successfully downloaded the MiniUtils library"
