aefd34e44271f8e913dba7fe1e190f8a3256d6f2
Josh14 <joshua.huizer@gmail.com>
2026-04-25 17:45:00 +0200

    Implement program installation from GitHub to the Test branch
    Add functionality to download and install programs from GitHub repository.

--- MiniInstaller.sh
+++ MiniInstaller.sh
@@ -11,6 +11,31 @@ Please note,
 All of these programs were made by a 12 year old
 And if you encounter any bugs, please contact me by commenting on the GitHub Repo
 EOF
+echo "looking for programs"
+mkdir -p /tmp/MiniInstaller
+wget -q -O "/tmp/MiniInstaller/repo.zip" "https://github.com/JoshCoder26/MiniUtils/archive/main.zip"
+unzip -q "/tmp/MiniInstaller/repo.zip" -d "/tmp/MiniInstaller"
+for file in "/tmp/MiniInstaller/MiniUtils-main"/*; do
+FILE="$(basename "$file")"
+if [[ -d "$file" ]]; then
+continue
+fi
+if [[ "$FILE" == "MiniInstaller.sh" || "$FILE" == "README.md" ]]; then
+continue
+fi
+if ! diff "$file" "/usr/local/bin/$FILE" > /dev/null 2>&1 ; then
+if [[ "$FILE" == "MiniUpdater" ]]; then
+cp "$file" "/usr/local/bin/$MiniUpdater"
+chmod 555 "/usr/local/bin/$MiniUpdater"
+continue
+fi
+read -p "Do you want to install/update $FILE? (Y/N)" INPUT
+if [[ ${INPUT,,} == "y" ]]; then
+cp "$file" "/usr/local/bin/$FILE"
+chmod 555 "/usr/local/bin/$FILE"
+fi
+fi
+done
 # read -p "Do you want to install MiniEditor? (Y/N)" INPUT
 # if [[ "${INPUT,,}" == "y" ]]; then
 # wget -q -O /usr/local/bin/MiniEditor https://raw.githubusercontent.com/JoshCoder26/MiniUtils/main/MiniEditor
